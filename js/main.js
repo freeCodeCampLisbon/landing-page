b2f472d043a9780b9787438d77bd8344371cfc12
Amrit Poudel <amritpoudel11@gmail.com>
2018-06-17 15:46:55 +0100

    add javascript functionality to the hamburger button

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const hamBurger = document.querySelector(".hamBurger")
+const lists = document.querySelector(".collapse");
+
+hamBurger.addEventListener("click", function(){
+    burgerFunction();
+}) 
+
+function burgerFunction() {
+    if (lists.style.display === "block") {
+        lists.style.display = "none";
+    } else {
+        lists.style.display = "block";
+        lists.style.textAlign = "right";
+    }
+}
